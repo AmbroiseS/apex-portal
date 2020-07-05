cb816e945202ee822bb93c22a4ec29ef52b6a1af
AmbroiseS <amb.soullier@gmail.com>
2020-07-05 10:54:05 -0400

    [Deploy] deploy script

---
+++ deploy/deploy.bat
@@ -0,0 +1,9 @@
+cd %..%
+call ng build --prod --baseHref="https://ambroises.github.io/apex-portal/"
+copy  "..\docs\index.html" "..\docs\404.html" /Y
+call git reset
+call git add ../docs/
+call git status
+PAUSE
+call git commit -m "[Deploy] --deploy by script--"
+call git push master origin
